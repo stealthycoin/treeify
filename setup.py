50ca5c569fecda26fcc5c02493846670629e1a26
stealthycoin <john.w.carlyle@gmail.com>
2017-03-18 15:37:52 -0700

    0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-version = '0.1'
+version = '0.2'
 
 install_requires = []
 description = "Renders recursive structures in a terminal friendly way."
@@ -12,7 +12,13 @@ setup(name='treeify',
       version=version,
       description=description,
       long_description=description,
-      classifiers=[],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
       keywords='',
       author='John Carlyle',
       author_email='treeify@jcarlyle.email',
